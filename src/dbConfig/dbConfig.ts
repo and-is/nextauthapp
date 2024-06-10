915a08bf0d7227328bcb3ef03b7e72785a63f426
and-is <andispaudel@gmail.com>
2024-06-10 14:07:09 +0545

    tested and fixed errors on signup route

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 export async function connect() {
   try {
-    mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(process.env.MONGO_URI!);
     const connection = mongoose.connection;
     connection.on("connected", () => {
       console.log("MongoDB connected");
     });
     connection.on("error", (error) => {
       console.log(
-        "Mongodb connection error, please make sure db is up and running" +
+        "Mongodb connection error, please make sure db is up and running " +
           error
       );
       process.exit();
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const userSchema = new Mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: [true, "Please provide a username"],
